a2bda74cb83aa30ff3bc9c9ec3c47c8d4db599f8
Unmesh Patil <punmesh56@gmail.com>
2026-04-06 09:29:05 +0530

    Add files via upload

---
+++ firstApp/Component/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      Hii this is my Navbar
+    </div>
+  )
+}
+
+export default Navbar
+
+// IMPORT THIS NAVBAR IN LAYOUT.JS JUST LIKE REACT
---
+++ firstApp/app/contact/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      Email : punmesh56@gmail.com
+    </div>
+  )
+}
+
+export default Contact
