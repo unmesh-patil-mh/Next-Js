37cfd28f6c97155df89eae43154784a890f72c13
Unmesh Patil <punmesh56@gmail.com>
2026-04-07 18:43:10 +0530

    Add files via upload

---
+++ ServerComponent/component/Navbar.js
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      I am an use Client Navbar and Page is Use Server component still I am Working
+    </div>
+  )
+}
+
+export default Navbar
